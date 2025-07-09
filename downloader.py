6c72184de6a55fd96027600435c0f72a113e09b1
sakhawathsumit <xshsumit@gmail.com>
2025-07-09 11:45:51 +0200

    simplify argument.
    use talktorial id and and predefined base urls to download files.

--- downloader.py
+++ downloader.py
@@ -8,7 +8,6 @@ def download_requirements(requirements_url,
     """
     Downloads the requirements.txt file from the specified URL.
     """
-    
     try:
         # Check if the requirements file already exists
         if os.path.exists(requirements_target):
@@ -32,25 +31,20 @@ def download_contents(content_url, target_folder):
     """
     try:
         print(f"Downloading {target_folder}...")
-
         # Create local target folder
         os.makedirs(target_folder, exist_ok=False)
-
         # Get list of files in the GitHub folder
         response = requests.get(content_url)
         response.raise_for_status()
         files = response.json()
-
         # Download each file
         for file_info in files:
             download_url = file_info['download_url']
             file_name = file_info['name']
             file_path = os.path.join(target_folder, file_name)
-
             print(f"Downloading {file_name}...")
             file_response = requests.get(download_url)
             file_response.raise_for_status()
-
             with open(file_path, 'wb') as f:
                 f.write(file_response.content)
 
@@ -63,21 +57,25 @@ def main():
     """
     Main function to run the downloader script.
     """
-    # Handle requirements.txt separately as it's a raw file
-    if len(sys.argv) == 2:
-        requirements_url = sys.argv[1]
-        requirements_target = 'requirements.txt'
-        download_requirements(requirements_url)
-
-    elif len(sys.argv) == 3:
-        content_url = sys.argv[1]
-        concent_folder = sys.argv[2]
-        download_contents(content_url, concent_folder)
-    
-    else:
-        print("Something went wrong. Please provide the correct arguments.")
+    if len(sys.argv) != 2:
+        print("Wrong arguments. e.g., T001_query_chembl")
         sys.exit(1)
+    
+    talktorial = sys.argv[1]
+    base_requirements_url = "https://raw.githubusercontent.com/volkamerlab/teachopencadd/sepenv/teachopencadd/talktorials"
+    base_data_url = "https://api.github.com/repos/volkamerlab/teachopencadd/contents/teachopencadd/talktorials"
+    base_images_url = "https://api.github.com/repos/volkamerlab/teachopencadd/contents/teachopencadd/talktorials"
+    data_folder = "data"
+    image_folder =  "images"
 
+    download_requirements(
+            f'{base_requirements_url}/{talktorial}/requirements.txt')
+    download_contents(
+            f'{base_images_url}/{talktorial}/{image_folder}',
+                    image_folder)
+    download_contents(
+            f'{base_data_url}/{talktorial}/{data_folder}',
+                    data_folder)
     print("Download completed successfully.")
 
 
