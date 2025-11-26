9d29634755d574136f2dffb6d4911e680547f9ed
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-11-26 10:08:48 +0100

    run script fails on test error

--- main.py
+++ main.py
@@ -24,14 +24,15 @@ def package_info(req_file):
         str : python version. e.g. '3.12.0'
         list : list of required packages. e.g. ['pandas', 'numpy',].
     """
-    with open(req_file, 'r') as f:
+    with open(req_file, "r") as f:
         pkg_info = f.read().strip()
-    py_vrs = pkg_info.split('\n')[0]
-    assert py_vrs.split('=')[0] == '#python', \
-        'First package in requirements file must be '\
-        'python with specified version. e.g. python=3.12.0'
+    py_vrs = pkg_info.split("\n")[0]
+    assert py_vrs.split("=")[0] == "#python", (
+        "First package in requirements file must be "
+        "python with specified version. e.g. python=3.12.0"
+    )
     py_vrs = re.findall(r"[\d.]+", py_vrs)[0]
-    pkgs_lst = pkg_info.split('\n')[1:] 
+    pkgs_lst = pkg_info.split("\n")[1:]
     return py_vrs, pkgs_lst
 
 
@@ -41,50 +42,51 @@ def conda_env_list():
     returns:
         list: List of conda environment names.
     """
-    result = subprocess.run(["conda", "env", "list"], 
-                            capture_output=True, text=True)
+    result = subprocess.run(["conda", "env", "list"], capture_output=True, text=True)
     if result.returncode != 0:
-        controlled_crash("Error listing conda environments: " + \
-                        result.stderr)
-    
+        controlled_crash("Error listing conda environments: " + result.stderr)
+
     envs = result.stdout.splitlines()
-    return [line.split()[0] for line in envs 
-            if line and not line.startswith("#")]
+    return [line.split()[0] for line in envs if line and not line.startswith("#")]
 
 
 def configure_env(prefix, python_version, req_file):
     """
-    Configure a conda environment with the specified 
+    Configure a conda environment with the specified
     python version and requirements.
-    
+
     Args:
         prefix (str): The prefix for the environment, e.g. 'T001'.
         python_version (str): The python version to use, e.g. '3.12.0'.
         req_file (str): requirements.txt file path.
     """
-    #env convention:TXXX_py* e.g. T001_312
-    env_name = prefix + '_' + python_version.replace('.', '')
+    # env convention:TXXX_py* e.g. T001_312
+    env_name = prefix + "_" + python_version.replace(".", "")
     existing_envs = conda_env_list()
     if not env_name in existing_envs:
-        print(f"Creating conda environment '{env_name}'\
-                 with Python {python_version}")
-        result = subprocess.run(["conda", "create", "-n", env_name, 
-                        f"python={python_version}", "--yes"], check=True,
-                        capture_output=True, text=True)
+        print(
+            f"Creating conda environment '{env_name}'\
+                 with Python {python_version}"
+        )
+        result = subprocess.run(
+            ["conda", "create", "-n", env_name, f"python={python_version}", "--yes"],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
         if result.returncode != 0:
-            controlled_crash("Error listing conda environments: "\
-                             + result.stderr)
+            controlled_crash("Error listing conda environments: " + result.stderr)
 
     print(f"Installing dependencies in '{env_name}'...")
-    #pkg_install_cmd = 'conda run -n '+ env_name + ' pip install '
-    #pkg_install_cmd += ' '.join(pkg_list)
-    req_file_install_cmd = 'conda run -n ' + env_name + ' pip install -r ' + req_file
+    # pkg_install_cmd = 'conda run -n '+ env_name + ' pip install '
+    # pkg_install_cmd += ' '.join(pkg_list)
+    req_file_install_cmd = "conda run -n " + env_name + " pip install -r " + req_file
     print(f"Running command: {req_file_install_cmd}")
-    result = subprocess.run(req_file_install_cmd, shell=True, 
-                            capture_output=True, text=True)
+    result = subprocess.run(
+        req_file_install_cmd, shell=True, capture_output=True, text=True
+    )
     if result.returncode != 0:
-            controlled_crash("Error installing dependencies: "\
-                             + result.stderr)
+        controlled_crash("Error installing dependencies: " + result.stderr)
     return env_name
 
 
@@ -95,17 +97,23 @@ def set_ipykernel(env_name):
     print(f"Setting ipykernel for '{env_name}'...")
     # install ipykernel
     result = subprocess.run(
-                        f"conda run -n {env_name} pip install ipykernel",
-                        shell=True, capture_output=True, text=True)
+        f"conda run -n {env_name} pip install ipykernel",
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
     if result.returncode != 0:
         controlled_crash("Error installing ipykernel: " + result.stderr)
     # set ipykernel
     result = subprocess.run(
-            f"conda run -n {env_name} python -m ipykernel install --user \
+        f"conda run -n {env_name} python -m ipykernel install --user \
             --name {env_name} --display-name '{env_name}'",
-            shell=True, capture_output=True, text=True)
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
     if result.returncode != 0:
-            controlled_crash("Error setting ipykernel: " + result.stderr)
+        controlled_crash("Error setting ipykernel: " + result.stderr)
     print(f"ipykernel for '{env_name}' set successfully.")
 
 
@@ -120,7 +128,7 @@ def set_nb_kernelspec(talktorial_dir: Path, env_name: str):
         nb.metadata.kernelspec = {
             "name": env_name.lower(),
             "display_name": env_name,
-            "language": "python"
+            "language": "python",
         }
         nbformat.write(nb, nb_path)
     else:
@@ -131,8 +139,10 @@ def find_talktorial_folder(txxx):
     search_pattern = BASE_DIR / f"{txxx}_*"
     matches = glob.glob(str(search_pattern))
     if not matches:
-        controlled_crash(f"No folder found for '{txxx}_*' \
-                        in '{BASE_DIR}'.")
+        controlled_crash(
+            f"No folder found for '{txxx}_*' \
+                        in '{BASE_DIR}'."
+        )
     return Path(matches[0])
 
 
@@ -141,8 +151,7 @@ def start_talktorial(talktorial_dir: Path, env_name: str):
     talktorial = talktorial_dir / TALKTORIAL_FILE
     if talktorial.exists():
         print(f"Starting talktorial {talktorial}")
-        notebook_cmd = f"conda run -n {env_name} jupyter notebook {
-                                                        str(talktorial)}"
+        notebook_cmd = f"conda run -n {env_name} jupyter notebook {str(talktorial)}"
         subprocess.run(notebook_cmd, shell=True)
     else:
         controlled_crash(f"Error: Notebook '{talktorial}' not found.")
@@ -154,13 +163,17 @@ def test_talktorial(talktorial_dir: Path, env_name: str):
     if talktorial.exists():
         print(f"Testing talktorial {talktorial}")
         result = subprocess.run(
-                        f"conda run -n {env_name} pip install pytest nbval",
-                        shell=True, capture_output=True, text=True)
+            f"conda run -n {env_name} pip install pytest nbval",
+            shell=True,
+            capture_output=True,
+            text=True,
+        )
         if result.returncode != 0:
             controlled_crash("Error installing pytest nbval: " + result.stderr)
 
         notebook_cmd = f"conda run --no-capture-output -n {env_name} pytest --nbval-lax {str(talktorial)}"
-        subprocess.run(notebook_cmd, shell=True)
+        result = subprocess.run(notebook_cmd, shell=True)
+        result.check_returncode()
     else:
         controlled_crash(f"Error: Notebook '{talktorial}' not found.")
 
@@ -175,14 +188,16 @@ def main(txxx, test_mode=False):
     talktorial_dir = find_talktorial_folder(txxx)
     print(f"Found talktorial folder: \n{talktorial_dir}")
     req_path = talktorial_dir / REQ_FILE
-    req_path = str(req_path) # convert posixpath to str
+    req_path = str(req_path)  # convert posixpath to str
     if not os.path.exists(req_path):
-        controlled_crash(f"Requirements file '{REQ_FILE}' \
-                            not found in {talktorial_dir}.")
+        controlled_crash(
+            f"Requirements file '{REQ_FILE}' \
+                            not found in {talktorial_dir}."
+        )
     python_version, pkg_list = package_info(req_path)
     print(f"Python version: {python_version}")
     print(f"Package list: \n{pkg_list}")
-    
+
     env_name = configure_env(txxx, python_version, req_path)
     print(f"Configured environment: {env_name}")
     set_ipykernel(env_name)
@@ -196,7 +211,9 @@ def main(txxx, test_mode=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="CLI.")
-    parser.add_argument("--talktorial", "-t", help="Taltorial to run, e.g., T001", required=True)
+    parser.add_argument(
+        "--talktorial", "-t", help="Taltorial to run, e.g., T001", required=True
+    )
     parser.add_argument("--test", default=False, help="run pytest.")
     args = parser.parse_args()
     main(args.talktorial, args.test)
