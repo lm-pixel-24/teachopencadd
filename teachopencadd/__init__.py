c288bc354a1bb0d0ec6b0dbdffbe04d21e275081
Michael Backenköhler <m.backenkoehler@gmail.com>
2026-03-26 11:37:46 +0100

    move from versioneer to setuptools_scm

--- teachopencadd/__init__.py
+++ teachopencadd/__init__.py
@@ -4,11 +4,3 @@ to help you learn or teach computer aided drug design concepts.
 
 The notebooks themselves are located under ``talktorials/``.
 """
-
-# Handle versioneer
-from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
